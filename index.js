9393e93fb03278bb8584e4189186aae694a57142
mouleshleo <mouleshleo06@gmail.com>
2025-03-25 22:18:04 +0530

    menu added

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 
 var swiper = new Swiper(".mySwiper", {
-    effect: "cards",  
-    grabCursor: true,   
+    effect: "cards",
+    grabCursor: true,
     initialSlide: 2,
-    cardsEffect: {
-        rotate: true,  
-        perSlideOffset: 18, 
-        perSlideRotate: 10,  
-    },
     autoplay: {
         delay: 5000,
     },
+    breakpoints: {
+        320: { 
+            cardsEffect: {
+                rotate: true,
+                perSlideOffset: 6,  
+                perSlideRotate: 3,  
+            },
+        },
+        768: { 
+            cardsEffect: {
+                rotate: true,
+                perSlideOffset: 15,
+                perSlideRotate: 8,
+            },
+        },
+        1024: {
+            cardsEffect: {
+                rotate: true,
+                perSlideOffset: 18,
+                perSlideRotate: 10,
+            },
+        },
+    },
 });
+
 
 let menu = document.querySelector(".menu");
 let mobileMenu = document.querySelector(".mobile-menu");
 
-menu.addEventListener("click", (e) => {
-    mobileMenu.classList.toggle("hidden");
+menu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("opacity-100");
+    mobileMenu.classList.toggle("translate-y-0");
+    mobileMenu.classList.toggle("pointer-events-auto");
 });
