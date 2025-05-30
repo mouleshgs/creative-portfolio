1ea510894fc8f884953b194f20f47dc7fe011454
mouleshleo <mouleshleo06@gmail.com>
2025-05-30 21:29:52 +0530

    backend added for contact page

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
+import path from "path";
 import mongoose from "mongoose";
-
+import Contact from "./models/Contact.js";
 
 dotenv.config();
 
@@ -16,7 +17,35 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-
 .then(() => console.log(`the db is connected at ${port}`))
 .catch((err) => console.log(`error : ${err}`));
 
+
+
+app.post("/contact", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    const newContact = new Contact({ name, email, message });
+    await newContact.save();
+    res.status(201).send("Message stored successfully!");
+  } catch (error) {
+    res.status(500).send("Error storing message.");
+  }
+});
+
+const __filename  = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, "..")));
+
+app.get(/^\/(?!contact).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../contact.html"));
+});
+
+
+app.listen(port, () => {
+  console.log(`🚀 Server running at http://localhost:${port}`);
+console.log(__dirname);
+
+});
---
+++ contact.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("#contact-form");
+
+form.addEventListener("submit", async (e) => {
+e.preventDefault();
+
+  const name = e.target.elements.name.value;
+  const email = e.target.elements.email.value;
+  const message = e.target.elements.message.value;
+
+const bodyJSON = JSON.stringify({ name, email, message });
+
+
+console.log(bodyJSON);
+
+
+try {
+    const response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json", 
+        },
+
+        body: bodyJSON,
+    });
+
+    if (response.ok) {
+        alert("Message is sent successfully!");
+        form.reset();
+    } else {
+        alert("Failed to send message.");
+
+    }
+} catch (error) {
+    console.log("error: ", error);
+    alert("Cannot reach the server now. try again later");
+    form.reset();
+}
+});
