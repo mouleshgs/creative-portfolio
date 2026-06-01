31d01d96241ea0641106a419000f2b5b6c90f3c9
moulesh <mouleshleo06@gmail.com>
2026-06-01 12:41:25 +0530

    javac

--- backend/server.js
+++ backend/server.js
@@ -8,35 +8,35 @@ import Contact from "./models/Contact.js";
 
 dotenv.config();
 
-
 const app = express();
-const port = 5000;
-
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log(`the db is connected at ${port}`))
-.catch((err) => console.log(`error : ${err}`));
-
-
-
 app.post("/contact", async (req, res) => {
-    console.log("POST /contact received:", req.body); 
+  console.log("POST /contact received:", req.body);
+
   try {
     const { name, email, message } = req.body;
-    const newContact = new Contact({ name, email, message });
+
+    const newContact = new Contact({
+      name,
+      email,
+      message,
+    });
+
     await newContact.save();
+
     res.status(201).send("Message stored successfully!");
   } catch (error) {
+    console.error("Save Error:", error);
     res.status(500).send("Error storing message.");
   }
 });
 
-const __filename  = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 app.use(express.static(path.join(__dirname, "..")));
 
@@ -44,9 +44,15 @@ app.get(/^\/(?!contact).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../contact.html"));
 });
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
 
-app.listen(port, () => {
-  console.log(`🚀 Server running at cloud ${port}`);
-console.log(__dirname);
-
-});
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+  });
